79d21a8038e9d90c3df1924f9312ad0457348956
Dzmitry Sankouski <dsankouski@gmail.com>
2025-01-13 17:32:20 +0300

    format code

--- 16f676_8b_led_blink.asm
+++ 16f676_8b_led_blink.asm
@@ -1,66 +1,66 @@
-	include "p16f676.inc"	;include the defaults for the chip
-	__config 0x3D14		;sets the configuration settings (oscillator type etc.)
-				; HERE SET TO INTERNAL OSCILLATOR 4MHZ
+include "p16f676.inc"	;include the defaults for the chip
+__config 0x3D14		;sets the configuration settings (oscillator type etc.)
+; HERE SET TO INTERNAL OSCILLATOR 4MHZ
 
-	cblock 	0x20 		;start of general purpose registers
-	    light_state
-	    milliseconds_L	;milliseconds amount low order 8 bit
-	    milliseconds_H	;milliseconds amount high order 8 bit
-	    is_measurement_in_process	
-	    ;max milliseconds amount is 2^16 - 1 = 65535
-	endc
-	
-	org	0x0004		;interrupt vector
-		goto	InterruptController
-	
-	org	0x0050		;org sets the origin, 0x0000 for the 16F628,
-				;this is where the program starts running	
+cblock 	0x20 		;start of general purpose registers
+light_state
+milliseconds_L	;milliseconds amount low order 8 bit
+milliseconds_H	;milliseconds amount high order 8 bit
+is_measurement_in_process	
+;max milliseconds amount is 2^16 - 1 = 65535
+endc
 
-	bcf 	STATUS,RP0 	;Bank 0
-	clrf 	PORTA 		;Init PORTA
-	bsf 	STATUS,RP0 	;Bank 1
-	clrf	ANSEL 		;digital I/O
-   	clrf 	TRISC
-	clrf	TRISA		;set PortA all outputs to 0
-	bcf	STATUS,	RP0	;select bank 0
-	
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	;;;;; Input pins setup
-	movlw	0x03		;
-	movwf	TRISA		;setting PORTA<0,1> as inputs
-	movwf	IOCA		;enabling PORTA<0,1> interrupts-on-change
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	;;;;; Timer setup. Registers TMR0 , INTCON, OPTION_REG
-	clrf	TMR0        ;setting timer0 duration
-	bsf	STATUS,RP0  ;Bank 1
-	;Prescaler setup
-	;setting timer0 prescaler, and assigning prescaler to timer0
-	;scaling is 256. Timer frequency is Fosc/(4*2*65536)
-	movlw   b'0000111'
-	movwf	OPTION_REG
-	;;;;;;End of scaling setup
-	bcf	STATUS,	RP0 ;select bank 0
-	;;;;;;End of timer setup
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	
-	;;;;; Interrupts setup
-	movlw   b'10001000'
-	movwf	INTCON      ;setting interrupt for input pins
+org	0x0004		;interrupt vector
+goto	InterruptController
 
-Loop	
-	nop				
-	nop				
-	goto	Loop			
+org	0x0050		;org sets the origin, 0x0000 for the 16F628,
+    ;this is where the program starts running	
 
-InterruptController
-	btfsc	INTCON,RAIF	    ;PORTA interrupt case
-	call	PinInterruptHandler
-	btfsc	INTCON,T0IE	    ;Timer0 interrupt case
-	call	TimerInterruptHandler
-	retfie
-	
+    bcf 	STATUS,RP0 	;Bank 0
+    clrf 	PORTA 		;Init PORTA
+    bsf 	STATUS,RP0 	;Bank 1
+    clrf	ANSEL 		;digital I/O
+    clrf 	TRISC
+    clrf	TRISA		;set PortA all outputs to 0
+    bcf	STATUS,	RP0	;select bank 0
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;; Input pins setup
+    movlw	0x03		;
+    movwf	TRISA		;setting PORTA<0,1> as inputs
+    movwf	IOCA		;enabling PORTA<0,1> interrupts-on-change
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;; Timer setup. Registers TMR0 , INTCON, OPTION_REG
+    clrf	TMR0        ;setting timer0 duration
+    bsf	STATUS,RP0  ;Bank 1
+    ;Prescaler setup
+    ;setting timer0 prescaler, and assigning prescaler to timer0
+    ;scaling is 256. Timer frequency is Fosc/(4*2*65536)
+    movlw   b'0000111'
+    movwf	OPTION_REG
+    ;;;;;;End of scaling setup
+    bcf	STATUS,	RP0 ;select bank 0
+    ;;;;;;End of timer setup
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    ;;;;; Interrupts setup
+    movlw   b'10001000'
+    movwf	INTCON      ;setting interrupt for input pins
+
+    Loop	
+    nop				
+    nop				
+    goto	Loop			
+
+    InterruptController
+    btfsc	INTCON,RAIF	    ;PORTA interrupt case
+    call	PinInterruptHandler
+    btfsc	INTCON,T0IE	    ;Timer0 interrupt case
+    call	TimerInterruptHandler
+    retfie
+
     incf    light_state		;increment light state
     btfsc   light_state,3	;additional scaling by 8. If Fosc=4Mhz, blinking period will be 1sec.
     movlw   0x3f		;if light state 3 bit is 0, turn on leds
@@ -72,27 +72,27 @@ InterruptController
     movlw   b'10100000' 
     movwf   INTCON		;setting interrupt for timer 0
     return	
-    
-PinInterruptHandler
+
+    PinInterruptHandler
     btfsc   is_measurement_in_process,0
     goto    StartMeasure
     btfss   is_measurement_in_process,0
     goto    EndMeasure
-	return
-    
-StartMeasure
-	clrf	milliseconds_L
-	clrf	milliseconds_H
-	movlw   b'10101000' 
+    return
+
+    StartMeasure
+    clrf	milliseconds_L
+    clrf	milliseconds_H
+    movlw   b'10101000' 
     movwf   INTCON		;enabling interrupts for both timer 0 and input pins
-	
-EndMeasure
-	movlw   b'10001000' 
+
+    EndMeasure
+    movlw   b'10001000' 
     movwf   INTCON		;enabling interrupts for and input pins
-    
-TimerInterruptHandler
-	incf	milliseconds_L
-	btfsc	STATUS,Z
-	incf	milliseconds_H
-	return
-end
+
+    TimerInterruptHandler
+    incf	milliseconds_L
+    btfsc	STATUS,Z
+    incf	milliseconds_H
+    return
+    end
